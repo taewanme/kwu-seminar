e64970857001fb78a9911dd30711760045d94195
Taewan Kim <taewanme@gmail.com>
2021-12-18 14:13:26 +0900

    init

---
+++ day3-crawler01.py
@@ -0,0 +1,48 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+
+for i in range(603001, 603056):
+    req = requests.get('https://www1.president.go.kr/petitions/'+str(i))
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    title = soup.select_one('#cont_view > div.cs_area > div.new_contents > div > div.petitionsView_left > div > h3')
+    title = title.text
+
+    agree_count = soup.select_one('#cont_view > div.cs_area > div.new_contents > div > div.petitionsView_left > div > h2 > span')
+    agree_count = agree_count.text
+    
+    category = soup.select_one('#cont_view > div.cs_area > div.new_contents > div > div.petitionsView_left > div > div.petitionsView_info > ul > li:nth-child(1)')
+    category = category.text
+    category = category[4:]
+
+    opendate = soup.select_one('#cont_view > div.cs_area > div.new_contents > div > div.petitionsView_left > div > div.petitionsView_info > ul > li:nth-child(2)')
+    opendate = opendate.text
+    opendate = opendate[4:]
+
+    closedate = soup.select_one('#cont_view > div.cs_area > div.new_contents > div > div.petitionsView_left > div > div.petitionsView_info > ul > li:nth-child(3)')
+    closedate = closedate.text
+    closedate = closedate[4:]
+
+    channel = soup.select_one('#cont_view > div.cs_area > div.new_contents > div > div.petitionsView_left > div > div.petitionsView_info > ul > li:nth-child(4)')
+    channel = channel.text
+    channel = channel[3:-6]
+
+    contents = soup.select_one('#cont_view > div.cs_area > div.new_contents > div > div.petitionsView_left > div > div.petitionsView_write > div.View_write')
+    contents = contents.text
+    escapes = '\b\n\r\t\\' 
+    for c in escapes: 
+        contents = contents.replace(c, '') 
+
+    data = {}
+    data["title"] = title
+    data["agree_count"] = agree_count
+    data["category"] = category
+    data["opendate"] = opendate
+    data["closedate"] = closedate
+    data["channel"] = channel
+    data["contents"] = contents
+
+    print(i, data)
+    time.sleep(1)
