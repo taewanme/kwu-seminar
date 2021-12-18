806010c8e245d7bbc4183cd755d9014e3cce2ed2
Taewan Kim <taewanme@gmail.com>
2021-12-18 16:36:41 +0900

    init

--- day3-config-kibana.sh
+++ day3-config-kibana.sh
@@ -1,4 +1,4 @@
-wget https://raw.githubusercontent.com/taewanme/kwu-seminar/main/3day-kibana.yml
+wget https://raw.githubusercontent.com/taewanme/kwu-seminar/main/day3-kibana.yml
 sudo rm -f /etc/kibana/kibana.yml
 sudo mv ./3day-kibana.yml /etc/kibana/kibana.yml
 sudo systemctl restart kibana
