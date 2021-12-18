e1fec7714035070a1c0391da4f37f6a7876f10b6
Taewan Kim <taewanme@gmail.com>
2021-12-18 18:26:03 +0900

    init

--- day3-es-plugin.sh
+++ day3-es-plugin.sh
@@ -1,1 +1,6 @@
 sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install analysis-nori
+sudo systemctl status elasticsearch
+sudo systemctl stop elasticsearch
+sudo systemctl status elasticsearch
+sudo systemctl start elasticsearch
+sudo systemctl status elasticsearch
