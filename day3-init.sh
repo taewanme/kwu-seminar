3b1e05861544290c0176be99756d2485b5024488
Taewan Kim <taewanme@gmail.com>
2021-12-18 14:17:38 +0900

    init

--- day3-init.sh
+++ day3-init.sh
@@ -60,14 +60,14 @@ sleep 5
 ./kafka/bin/kafka-topics.sh \
 --create --bootstrap-server localhost:9092,localhost:9093,localhost:9094 \
 --replication-factor 3 --partitions 3 \
---topic crawling_topic
+--topic crawlingtopic
 
 ###################################
 ## Starting Console-Consumer
 ###################################
 ./kafka/bin/kafka-console-consumer.sh \
 --bootstrap-server localhost:9092,localhost:9093,localhost:9094 \
---topic crawling_topic --from-beginning
+--topic crawlingtopic --from-beginning
 
 
 
